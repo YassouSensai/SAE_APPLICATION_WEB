8c4a33839b0817230a20154afa21eef3bdcd9d74
Ostap <ostap.tymchyshyn.92@gmail.com>
2023-11-06 22:13:44 +0100

    Créations : ajout des nouveaux fichier action_admin_web.php et admin_web.php, les fichiers qui sont en relation avec l'administrateur web
    Modifications : lègeres modifications dans le css pour permettre d'afficher mieux les blocks et erreures
    
    - modifications de la page profil.html pour savoir ce qu'il nous faudra coder dedans dans le futur
    -modification de la page inscription.php qui permet de s'incrcrire(avait pas le mme affichage que la page connexion.php)

---
+++ src/PHP/admin_web.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Administration Web</title>
+</head>
+<body>
+<h1>Administration Web</h1>
+
+<?php
+// pour ajouter le code pour afficher le formulaire de gestion des libellés, statuts, niveaux d'urgences, techniciens, etc.
+
+
+// peut etre ajouter des formulaires pour permettre à l'administrateur web d'ajouter, modifier ou supprimer ces informations.
+?>
+
+<a href="action_admin.php">Se déconnecter</a>
+</body>
+</html>
