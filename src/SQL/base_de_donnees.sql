b95918187e3ecd786464753175818b7913c1cb27
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2023-11-08 22:28:51 +0100

    base_de_donnees.sql : ne fonctionnait pas, donc modification afin que le script puisse fonctionner
    css_site_statique.css : tentative de modifications concernant le menu déroulant dans le formulaire d'inscription (infructueuse)
    
    entete.html : modification relative au changement de répertoire de certains fichiers

--- src/SQL/base_de_donnees.sql
+++ src/SQL/base_de_donnees.sql
@@ -52,10 +52,11 @@ DROP TABLE IF EXISTS Utilisateur;
 -- Table Utilisateurs :
 CREATE TABLE Utilisateur (
     id_util INTEGER PRIMARY KEY AUTO_INCREMENT,
+    identifiant VARCHAR(20) NOT NULL,
     nom_util VARCHAR(50) NOT NULL,
     prenom_util VARCHAR(50) NOT NULL,
     email_util VARCHAR(100) NOT NULL UNIQUE,
-    mdp_util VARCHAR(20) NOT NULL,
+    mdp VARCHAR(20) NOT NULL,
     type_util VARCHAR(30) NOT NULL
 );
 
@@ -64,8 +65,8 @@ CREATE TABLE AdminSysteme (
     id_adminsys INTEGER PRIMARY KEY AUTO_INCREMENT,
     nom_adminsys VARCHAR(50) NOT NULL,
     prenom_adminsys VARCHAR(50) NOT NULL,
-    identifiant_adminsys VARCHAR(30) NOT NULL,
-    mdp_adminsys VARCHAR(20) NOT NULL
+    identifiant VARCHAR(30) NOT NULL,
+    mdp VARCHAR(20) NOT NULL
 );
 
 -- Table Administrateurs_Web :
@@ -73,8 +74,8 @@ CREATE TABLE AdminWeb (
     id_adminw INTEGER PRIMARY KEY AUTO_INCREMENT,
     nom_adminw VARCHAR(50) NOT NULL,
     prenom_adminw VARCHAR(50) NOT NULL,
-    identifiant_adminw VARCHAR(30) NOT NULL,
-    mdp_adminw VARCHAR(20) NOT NULL
+    identifiant VARCHAR(30) NOT NULL,
+    mdp VARCHAR(20) NOT NULL
 );
 
 -- Table Techniciens :
@@ -82,8 +83,8 @@ CREATE TABLE Technicien (
     id_tech INTEGER PRIMARY KEY AUTO_INCREMENT,
     nom_tech VARCHAR(50) NOT NULL,
     prenom_tech VARCHAR(50) NOT NULL,
-    identifiant_tech VARCHAR(30) NOT NULL,
-    mdp_tech VARCHAR(20) NOT NULL
+    identifiant VARCHAR(30) NOT NULL,
+    mdp VARCHAR(20) NOT NULL
 );
 -- Table Catégories de Problème :
 CREATE TABLE CategorieProbleme (
@@ -111,21 +112,21 @@ CREATE TABLE HistoriqueTickets (
 );
 
 
+
 -- Table Tickets :
 CREATE TABLE Ticket (
-    id_tic INTEGER PRIMARY KEY AUTO_INCREMENT,
-    desc_pb_tic VARCHAR(255) NOT NULL,
-    date_crea_tic DATE DEFAULT CURRENT_DATE NOT NULL,
-    date_maj_tic DATE,
-    createur_tic INTEGER NOT NULL REFERENCES Utilisateur(id_util),
-    tech_charge_tic INTEGER NOT NULL REFERENCES Technicien(id_tech),
-    status_tic INTEGER NOT NULL REFERENCES StatutTicket(id_status_tic),
-    nv_urgence_tic INTEGER NOT NULL REFERENCES NiveauUrgence(id_nv_urgence)
+                        id_tic INTEGER PRIMARY KEY AUTO_INCREMENT,
+                        desc_pb_tic VARCHAR(255) NOT NULL,
+                        date_crea_tic DATE DEFAULT CURRENT_DATE NOT NULL,
+                        date_maj_tic DATE,
+                        createur_tic INTEGER NOT NULL REFERENCES Utilisateur(id_util),
+                        tech_charge_tic INTEGER NOT NULL REFERENCES Technicien(id_tech),
+                        status_tic INTEGER NOT NULL REFERENCES StatutTicket(id_status_tic),
+                        nv_urgence_tic INTEGER NOT NULL REFERENCES NiveauUrgence(id_nv_urgence)
 );
 
---TRIGGERS :
+-- TRIGGER :
 DELIMITER //
-
 CREATE TRIGGER trig_check_urgence
     BEFORE INSERT ON Ticket
     FOR EACH ROW
@@ -135,51 +136,47 @@ BEGIN
 END IF;
 END;
 //
-
 DELIMITER ;
 
 
---TESTS:
 
 -- Insérer des données fictives dans la table Utilisateur
-INSERT INTO Utilisateur (id_util, nom_util, prenom_util, email_util, mdp_util, type_util) VALUES
-(1, 'Doe', 'John', 'john.doe@example.com', 'motdepasse1', 'Utilisateur'),
-(2, 'Smith', 'Alice', 'alice.smith@example.com', 'motdepasse2', 'Utilisateur'),
-(3, 'Johnson', 'Bob', 'bob.johnson@example.com', 'motdepasse3', 'Utilisateur');
+INSERT INTO Utilisateur (id_util, identifiant, nom_util, prenom_util, email_util, mdp, type_util) VALUES
+(1,'util1', 'Doe', 'John', 'john.doe@example.com', 'motdepasse1', 'Utilisateur'),
+(2,'util2', 'Smith', 'Alice', 'alice.smith@example.com', 'motdepasse2', 'Utilisateur'),
+(3,'util3', 'Johnson', 'Bob', 'bob.johnson@example.com', 'motdepasse3', 'Utilisateur');
 
 -- Insérer des données fictives dans la table AdminSysteme
-INSERT INTO AdminSysteme (id_adminsys, nom_adminsys, prenom_adminsys, identifiant_adminsys, mdp_adminsys) VALUES
-(1, 'AdminSys1', 'AdminSys1', 'adminsys1', 'motdepasseadmin1'),
-(2, 'AdminSys2', 'AdminSys2', 'adminsys2', 'motdepasseadmin2');
+INSERT INTO AdminSysteme (id_adminsys, nom_adminsys, prenom_adminsys, identifiant, mdp) VALUES
+    (1, 'AdminSys1', 'AdminSys1', 'adminsys1', 'motdepasseadmin1');
 
 -- Insérer des données fictives dans la table AdminWeb
-INSERT INTO AdminWeb (id_adminw, nom_adminw, prenom_adminw, identifiant_adminw, mdp_adminw) VALUES
-(1, 'AdminWeb1', 'AdminWeb1', 'adminweb1', 'motdepasseadminweb1'),
-(2, 'AdminWeb2', 'AdminWeb2', 'adminweb2', 'motdepasseadminweb2');
+INSERT INTO AdminWeb (id_adminw, nom_adminw, prenom_adminw, identifiant, mdp) VALUES
+    (1, 'AdminWeb1', 'AdminWeb1', 'adminweb1', 'motdepasseadminweb1');
 
 -- Insérer des données fictives dans la table Technicien
-INSERT INTO Technicien (id_tech, nom_tech, prenom_tech, identifiant_tech, mdp_tech) VALUES
-(1, 'Tech1', 'Tech1', 'tech1', 'motdepassetech1'),
-(2, 'Tech2', 'Tech2', 'tech2', 'motdepassetech2');
+INSERT INTO Technicien (id_tech, nom_tech, prenom_tech, identifiant, mdp) VALUES
+                                                                                        (1, 'Tech1', 'Tech1', 'tech1', 'motdepassetech1'),
+                                                                                        (2, 'Tech2', 'Tech2', 'tech2', 'motdepassetech2');
 
 -- Insérer des données fictives dans la table CatégorieProbleme
 INSERT INTO CategorieProbleme (id_cat_pb, libelle_cat_pb) VALUES
-(1, 'Catégorie 1'),
-(2, 'Catégorie 2'),
-(3, 'Catégorie 3');
+                                                              (1, 'Catégorie 1'),
+                                                              (2, 'Catégorie 2'),
+                                                              (3, 'Catégorie 3');
 
 -- Insérer des données fictives dans la table StatutTicket
 INSERT INTO StatutTicket (id_status_tic, libelle_status_tic) VALUES
-(1, 'Ouvert'),
-(2, 'Fermé'),
-(3, 'En attente');
+                                                                 (1, 'Ouvert'),
+                                                                 (2, 'Fermé'),
+                                                                 (3, 'En attente');
 
 -- Insérer des données fictives dans la table NiveauUrgence
 INSERT INTO NiveauUrgence (id_nv_urgence, libelle_nv_urgence) VALUES
-(1, 'Urgence faible'),
-(2, 'Urgence modérée'),
-(3, 'Urgence élevée'),
-(4, 'Urgence critique');
+                                                                  (1, 'Urgence faible'),
+                                                                  (2, 'Urgence modérée'),
+                                                                  (3, 'Urgence élevée'),
+                                                                  (4, 'Urgence critique');
 
 -- Insérer des données fictives dans la table Ticket
 INSERT INTO Ticket (id_tic, desc_pb_tic, createur_tic, tech_charge_tic, status_tic, nv_urgence_tic, date_maj_tic)
@@ -189,20 +186,11 @@ VALUES (1, 'Problème 1', 1, 1, 1, 3, CURRENT_DATE),
 
 -- Insérer des données fictives dans la table HistoriqueTickets
 INSERT INTO HistoriqueTickets (id_histtic, archive_tic) VALUES
-(1, 1),
-(2, 1),
-(3, 0);
+                                                            (1, 1),
+                                                            (2, 1),
+                                                            (3, 0);
 
-
-
-
---ON AFFICHE LES TABLES:
-
-GRANT SELECT ON ma_base_de_donnees.ma_table TO user;
-GRANT SELECT, ADD, INSERT, DELETE, UPDATE ON ticket TO admin;
-GRANT UPDATE
-
-
+-- AFFICHER LES TABLES :
 SELECT * FROM Utilisateur;
 SELECT * FROM AdminSysteme;
 SELECT * FROM AdminWeb;
@@ -212,7 +200,7 @@ SELECT * FROM CategorieProbleme;
 SELECT * FROM StatutTicket;
 SELECT * FROM NiveauUrgence;
 SELECT * FROM HistoriqueTickets;
-SELECT * FROM AdresseIP;
+
 
 
 
