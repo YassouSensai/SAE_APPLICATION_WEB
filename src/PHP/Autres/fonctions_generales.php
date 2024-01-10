514940ef7ab2ae11c9aada6988926025bf816de7
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2024-01-10 23:43:44 +0100

    Correction d'erreurs de sur-definition des fonctions dans fonctions_generales.php
    + Remise des messages d'erreurs/succes comme avant car mieux dans css_site_dynamique.css

--- src/PHP/Autres/fonctions_generales.php
+++ src/PHP/Autres/fonctions_generales.php
@@ -9,98 +9,121 @@
  * - La fonction oeilMdp() qui permet d'afficher l'input des mots de passes avec l'œil
  * */
 
-function connectDB() {
-    $serveur = "localhost";
-    $utilisateur = "user_sae";
-    $mot_de_passe = "azerty";
-    $base_de_donnees = "sae_bd";
+if (!function_exists('connectDB')) {
+    function connectDB() {
+        $serveur = "localhost";
+        $utilisateur = "user_sae";
+        $mot_de_passe = "azerty";
+        $base_de_donnees = "sae_bd";
 
-    $connexion = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
+        $connexion = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
 
-    if (!$connexion) {
-        die("La connexion a échoué : " . mysqli_connect_error());
-    }
-
-    return $connexion;
-}
-
-function prepareAndExecute($connexion, $query, $params = null) {
-    $prep = mysqli_prepare($connexion, $query);
-
-    if (!$prep) {
-        die("La préparation de la requête a échoué : " . mysqli_error($connexion));
-    }
-
-    if ($params) {
-        mysqli_stmt_bind_param($prep, ...$params);
-    }
-
-    mysqli_stmt_execute($prep);
-
-    return mysqli_stmt_get_result($prep);
-}
-
-function logActivity($username, $type, $description) {
-    $connexion = connectDB();
-    $query = "INSERT INTO JournalActivite (date_activite, adresse_ip, id_utilisateur, type_activite, description_activite) VALUES (NOW(), ?, ?, ?, ?)";
-
-
-    if (isset($_SESSION['table_user'])) {
-        if ($_SESSION['table_user'] == 'Utilisateur') {
-            $params = ["ssis", $_SERVER['REMOTE_ADDR'], $username, $type, $description];
-            prepareAndExecute($connexion, $query, $params);
-        } else {
-            $params = ["ssis", $_SERVER['REMOTE_ADDR'], 'NULL', $type, $description];
-            prepareAndExecute($connexion, $query, $params);
+        if (!$connexion) {
+            die("La connexion a échoué : " . mysqli_connect_error());
         }
+
+        return $connexion;
     }
-
-    mysqli_close($connexion);
 }
 
-function captchaForm() {
-    $nb1 = rand(1,10);
-    $nb2 = rand(1,20);
+if (!function_exists('prepareAndExecute')) {
+    function prepareAndExecute($connexion, $query, $params = null)
+    {
+        $prep = mysqli_prepare($connexion, $query);
 
-    $_SESSION['nb1'] = $nb1;
-    $_SESSION['nb2'] = $nb2;
+        if (!$prep) {
+            die("La préparation de la requête a échoué : " . mysqli_error($connexion));
+        }
 
-    echo "<label for='captcha'>Captcha : ".$nb1." x ".$nb2." = ? (requis)</label>";
-    echo "<input id='captcha' type='number' name='captcha' placeholder='Résultat' required>";
-}
+        if ($params) {
+            mysqli_stmt_bind_param($prep, ...$params);
+        }
 
-function verifCaptcha($result_captcha) {
-    $nb1 = $_SESSION['nb1'];
-    $nb2 = $_SESSION['nb2'];
-    $result = $nb1 * $nb2;
+        mysqli_stmt_execute($prep);
 
-    if ($result == $result_captcha) {
-        return true;
-    } else {
-        return false;
+        return mysqli_stmt_get_result($prep);
     }
-
 }
 
-function scriptsMdp() {
-    echo "<script src='https://unpkg.com/feather-icons'></script>
-              <script>
-                    feather.replace();
-              </script>
-              <script src='../../JS/script_oeil_mdp.js'></script>";
+
+if (!function_exists('logActivity')) {
+    function logActivity($username, $type, $description)
+    {
+        $connexion = connectDB();
+        $query = "INSERT INTO JournalActivite (date_activite, adresse_ip, id_utilisateur, type_activite, description_activite) VALUES (NOW(), ?, ?, ?, ?)";
+
+
+        if (isset($_SESSION['table_user'])) {
+            if ($_SESSION['table_user'] == 'Utilisateur') {
+                $params = ["ssis", $_SERVER['REMOTE_ADDR'], $username, $type, $description];
+                prepareAndExecute($connexion, $query, $params);
+            } else {
+                $params = ["ssis", $_SERVER['REMOTE_ADDR'], 'NULL', $type, $description];
+                prepareAndExecute($connexion, $query, $params);
+            }
+        }
+
+        mysqli_close($connexion);
+    }
 }
 
-function oeilMdp($id, $name, $placeholder, $required = true) {
-    echo "<label for='$id'>Votre mot de passe:</label>";
-    echo "<label class='password-container'>";
-    echo "<input type='password' id='$id' name='$name' placeholder='$placeholder' " . ($required ? 'required' : '') . "><br>";
-    echo "<div class='password-icon'>";
-    echo "<i data-feather='eye'></i>";
-    echo "<i data-feather='eye-off'></i>";
-    echo "</div>";
-    echo "</label>";
+if (!function_exists('captchaForm')) {
+    function captchaForm()
+    {
+        $nb1 = rand(1, 10);
+        $nb2 = rand(1, 20);
 
-    scriptsMdp();
+        $_SESSION['nb1'] = $nb1;
+        $_SESSION['nb2'] = $nb2;
+
+        echo "<label for='captcha'>Captcha : " . $nb1 . " x " . $nb2 . " = ? (requis)</label>";
+        echo "<input id='captcha' type='number' name='captcha' placeholder='Résultat' required>";
+    }
+}
+
+
+if (!function_exists('verifCaptcha')) {
+    function verifCaptcha($result_captcha)
+    {
+        $nb1 = $_SESSION['nb1'];
+        $nb2 = $_SESSION['nb2'];
+        $result = $nb1 * $nb2;
+
+        if ($result == $result_captcha) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
+
+if (!function_exists('scriptsMdp')) {
+    function scriptsMdp()
+    {
+        echo "<script src='https://unpkg.com/feather-icons'></script>
+                  <script>
+                        feather.replace();
+                  </script>
+                  <script src='../../JS/script_oeil_mdp.js'></script>";
+    }
+}
+
+
+if (!function_exists('oeilMdp')) {
+    function oeilMdp($id, $name, $placeholder, $required = true)
+    {
+        echo "<label for='$id'>Votre mot de passe:</label>";
+        echo "<label class='password-container'>";
+        echo "<input type='password' id='$id' name='$name' placeholder='$placeholder' " . ($required ? 'required' : '') . "><br>";
+        echo "<div class='password-icon'>";
+        echo "<i data-feather='eye'></i>";
+        echo "<i data-feather='eye-off'></i>";
+        echo "</div>";
+        echo "</label>";
+
+        scriptsMdp();
+    }
 }
 
 
