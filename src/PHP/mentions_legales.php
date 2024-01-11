3d2febd2d617e25ab406b7e7eed87af375612313
22203744 <ostap.tymchyshyn.92@gmail.com>
2024-01-11 11:49:18 +0100

    Ecriture de la page mentions_legales en PHP cette fois car la page étais en HTML et ne correspondais pas au style du site.
    Par conséquant : modification des lines vers la page

---
+++ src/PHP/mentions_legales.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../CSS/css_site_statique.css">
+    <title>Mentions Légales</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Les mentions légales contiennent des informations que tout site web ou service en ligne doit fournir pour se conformer à la réglementation en vigueur">
+    <meta name="keywords" content="mentions légales">
+    <meta name="author" content="TYMCHYSHYN Ostap, Elkhalki Yassine, Husleag Aaron">
+</head>
+<body>
+<?php
+include('../HTML/entete_general.html');
+?>
+
+<section>
+    <div class="Toute-la-page">
+        <div class="bas-page">
+            <div class="block2-transparent">
+                <h3 style="color: #FFFFFF">Introduction</h3>
+                <p>Le RGPD (Règlement Général sur la Protection des Données) est un règlement européen entré en vigueur le 25 mai 2018. Il a été conçu en ayant pour objectif la protection des données personnelles des citoyens de l'Union européenne et pour harmoniser les règles de protection des données au sein de l'UE. L’ancienne loi sur la protection des données de 1995 a donc été remplacée par le RGPD qui est plus adapté au contexte actuel d’une part parce qu’elle est appliqué de la même manière dans chaque pays inclu dans l’union européenne, d’autre part, l'intégration des règles de protection des données se fait dès la conception des divers sites internet et logiciels. Enfin, car les amendes en cas de non-respect sont bien plus importantes ce qui poussent les petites et grandes entreprises du marché à respecter la vie privée des citoyens européen avec plus de sérieux. Finalement le RGPD a également pour objectif de répondre aux défis posés par l'évolution rapide des technologies de l'information et de la communication.
+                    <br>
+                    <br>
+                    Le RGPD définit donc plusieurs règles et but à atteindre dans l’application de celui-ci, dont :
+                    <br>
+                    <br>
+                    La protection des données personnelles : Le RGPD garantit que les informations personnelles des individus soient traitées de manière licite, loyale et transparente au regard de la personne concernée.
+                    Le renforcement des droits des individus : Il accorde aux internautes plus de contrôle sur leurs données personnelles. En d’autre termes, grâce au RGPD, ils ont le droit de connaître quelles données sont collectées, quelles en sont leur utilisation, de demander l'accès à leurs données, de les corriger, de les supprimer (dans certaines conditions), et de s'opposer (éventuellement) à leur utilisation.
+                    Rendre les organisations plus responsables : Les organisations qui traitent les données personnelles de ses utilisateurs sont tenues de respecter des obligations strictes dans le cadre de la protection des données. Elles doivent adopter des pratiques de “privacy by design” et “security by default”, ce qui signifie que la protection des données doit être prise en compte dès la conception des diﬀérents services.
+                    Une réglementation à respecter absolument : Le RGPD introduit également des sanctions plus sévères notamment délivrées par la CNIL (Commission nationale de
+                    <br>
+                    <br>
+                    l’Informatique et des libertés). Les entreprises qui ne se conforment pas aux règles de protection des données risquent des amendes considérables, pouvant atteindre jusqu'à 4 % du chiﬀre d'aﬀaires annuel mondial de l'entreprise. De plus, le RGPD oblige les entreprises à notifier les violations de données personnelles aux autorités et personnes concernées après avoir pris connaissance de l'incident.
+                    <br>
+                    <br>
+                    Exemple : Facebook écope d’une amende de 1,2 milliards d’euros pour violation du règlement général sur la protection des données personnelles (source: Le monde informatique)
+                    <br>
+                    <br>
+                    Pour résumer, le RGPD a pour but de renforcer la protection des données personnelles, d'accorder aux individus un plus grand contrôle sur les informations, et de rendre les organisations qui traitent ces données davantage responsables. Il a et a eu par le passé un impact significatif sur la gestion et la protection des données personnelles par les entreprises, et il vise à favoriser la confiance dans le marché de l’informatique et du numérique. En découle du RGPD plusieurs principes ayant pour but la confidentialité des données dont le principe de “privacy by design” qui implique une prise en compte de la protection des données des utilisateurs avant de concevoir les applications, logiciels et sites. ( Selon l’article 25 du RGPD et d’après <a href="https://www.legalplace.fr/guides/privacy-by-design-rgpd/" style="color: #a9a9a9">legalPlace</a> )</p>
+            </div>
+        </div>
+    </div>
+    <div class="clear"></div>
+</section>
+<br>
+<br>
+<section>
+    <div class="Toute-la-page">
+        <div>
+            <h3>2 )Cartographie du traitement des données personnelles</h3>
+
+            <p>“Pour mesurer concrètement l’impact du règlement européen sur la protection des données de votre activité, commencez par recenser de façon précise les traitements de données personnelles que vous mettez en œuvre. La tenue d’un registre des traitements vous permet de faire le point.” Telles sont les recommandations de la CNIL ( <a href="https://www.cnil.fr/fr/cartographier-vos-traitements-de-donnees-personnelles" style="color: #a9a9a9">CNIL - Cartographie</a> ) concernant l’application du principe de Privacy by design pour la conception et le développement de notre application web. Ainsi, nous vous faisons part de la nature du traitement mis en œuvre, de sa finalité, de la catégorie des données traitées ainsi que la durée de conservation des données.
+            </p>
+            <p>
+                <strong> A) Nature du traitement mis en oeuvre :</strong>
+                Le traitement de données personnelles que nous avons mis en œuvre pour notre application web consiste à récupérer et enregistrer des données personnelles essentielles pour notre application tels que le nom, prénom et adresse mail. De cette manière on garantit une meilleure expérience utilisateur car ce dernier n’a qu’à fournir la quantité minimale d'informations. Ces informations sont généralement celles nécessaires pour l’inscription sur n’importe quel site internet et cela permet à l’application de travailler avec une quantité de données restreinte et à terme, rendre l’application plus performante. Parallèlement, la faible quantité d’informations récoltées signifie également qu’il y aura moins de données sensibles à protéger. Cela peut réduire les risques liés à la sécurité et les préoccupations des utilisateurs concernant la confidentialité de leurs données.
+            </p>
+            <p>
+                <strong> B) Finalité du traitement :</strong>
+                Nous utilisons les données récupérées pour gérer plus facilement les comptes utilisateurs. Les données personnelles sont donc utilisées pour créer et gérer les comptes utilisateurs, savoir qui a créé un ticket sur l’application ou encore connaître les habitudes des utilisateurs et savoir pourquoi certaines machines tombent souvent en panne. Les données
+                récupérées nous permettent également de gérer les autorisations pour les utilisateurs qui s’inscrivent et pour les utilisateurs qui font partie de l’équipe encadrante de la plateforme (administrateur web, administrateur système et techniciens.
+            </p>
+            <p>
+                <strong> C) Catégorie des données traitées :</strong>
+                Dans le cas de notre application, les données personnelles traitées sont destinées à
+                l’identification (nom, prénom & adresse mail). L’adresse mail est d’ailleurs la donnée la plus importante puisqu’elle est utilisée pour diﬀérencier les comptes utilisateurs puisque chaque adresse mail ne peut être reliée qu’à un seul compte à la fois. Le nom et prénom sont eux utilisés uniquement pour l’attribution des tickets puisque chaque ticket a un propriétaire.
+                <br>
+                <br>
+                Également, par la suite, nous proposerons aux clients qui le souhaitent de rentrer plus d’informations comme leur adresse, leur numéro de téléphone ainsi que leur genre. Ces données seront récoltées uniquement si les clients le souhaitent et seront utilisées à des fins statistiques. ( Cette récolte d’information n’est pas encore disponibles )
+            </p>
+            <p>
+                <strong> D) Durée de conservation :</strong>
+                Dans le cas de notre application, les données personnelles utilisées sont eﬀectivement utiles pour les l’identification des utilisateurs, les données sont gardées pendant la durée de vie du compte tant qu’il est actif. Si un utilisateur décide de supprimer son compte, les données peuvent être supprimées immédiatement ou dans un délai relativement court après une demande de suppression faite par l’utilisateur concerné. Selon la CNIL, cette durée peut atteindre 10 ans après la suppression du compte ( <a href="https://www.cnil.fr/fr/passer-laction/les-durees-de-conservation-des-donnees" style="color: #a9a9a9">CNIL - Durée</a> )
+            </p>
+        </div>
+    </div>
+    <div class="clear"></div>
+</section>
+<br>
+<br>
+<section>
+    <div class="Toute-la-page">
+        <div class="bas-page">
+            <div class="block2">
+                <h2 style="color: #FFFFFF">3 ) Gestion des droits des personnes concernées</h2>
+                <p>
+                    Nous récupérons les données personnelles des individus à des fins mentionnées plus haut. Ainsi, nous avons le devoir de nous assurer que ces individus soient en possessions de leurs droits vis à vis de leur données personnelles et en accord avec le RGPD. Les diﬀérents droits sont les suivants :
+                </p>
+                <ul  class="styled-list">
+                    <li>
+                        <strong>Le droit d'accès :</strong><br>
+                        Nos utilisateurs pourront être en mesure d'accéder à leur données personnelles stockées dans l'application. Pour cela nous proposerons une page réservée au compte utilisateur dans laquelle cette action sera possible. Les utilisateurs
+                        ne pourront accéder qu'aux données les concernant il ne seront pas en mesure d'accéder aux données des autres.
+                    </li>
+                    <li>
+                        <strong>Le droit de rectification :</strong><br>
+                        Les utilisateurs auront également le droit de corriger leurs données personnelles si elles sont inexactes ou s'ils changent d’adresse mail par exemple. En permettant aux utilisateurs de corriger les informations incorrectes, ils peuvent protéger leur réputation en ligne et hors ligne.                    </li>
+                    <li>
+                        <strong>Le droit à l'effacement : </strong><br>
+                        Les utilisateurs pourront aussi demander à ce que leurs données personnelles soient supprimées de nos bases de données. Ce droit améliore la protection de la vie privée en permettant aux individus de supprimer des informations qu’ils jugent sensibles, obsolètes ou qui pourraient être utilisées de manière inappropriée. Néanmoins, si les données qui doivent être supprimées sont nécessaires à nos activités (Exemple: nom et adresse mail pour l’identification des tickets),
+
+                        l’utilisateur pourra eﬀectivement supprimer ces données sous la contrainte de ne plus utiliser nos services.
+                    </li>
+                    <li>
+                        <strong>Le droit à la portabilité des données :</strong><br>
+                        Les utilisateurs pourront demander la récupération de leurs données personnelles sous la forme d’un document qu'ils pourront télécharger. Par ailleurs, le droit à la portabilité renforce la transparence des pratiques des entreprises en ce qui concerne le traitement des données. Les utilisateurs sont plus enclins à faire confiance à des entreprises qui respectent ce droit.
+                    </li>
+                    <li>
+                        <strong>Le droit d'opposition : </strong><br>
+                        Les utilisateurs ont le droit de s'opposer au traitement de leurs données personnelles et pourront nous le faire part par mail. Le droit d'opposition permet aux utilisateurs de contrôler la manière dont leurs données personnelles seront utilisées, renforçant ainsi le respect de leur vie privée. Cela donne aux individus un plus grand contrôle sur le traitement de leurs informations.                    </li>
+                </ul>
+                <p>
+                    En sommes nous comptons garantir ces droits ci-dessus à chaque utilisateur en ce qui concerne leur informations personnelles.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<br>
+<br>
+<section>
+    <div class="Toute-la-page">
+        <div class="block-contenu-en-ligne">
+            <h3>4 ) Sécurité des données personnelles</h3>
+            <p>
+                Dans le cadre de notre engagement envers la protection de la vie privée de nos utilisateurs, nous avons mis en place des mesures permettant de garantir la sécurité de leurs données personnelles. L'accès aux informations est strictement régulé par un système d'identification nécessitant un identifiant utilisateur ainsi qu'un mot de passe. De cette manière, nous garantissons que seuls les utilisateurs autorisés peuvent accéder à leurs données, garantissant ainsi la confidentialité de ces informations. De plus, toutes les données collectées sont soumises à un processus de cryptage avancé. Cette étape garantit que même en cas d'accès non autorisé, les informations demeurent illisibles et inutilisables par des tiers. Le
+
+                cryptage des données est une barrière supplémentaire, renforçant la sécurité globale du système. En somme, nous nous engageons à mettre en place les mesures nécessaires à la sécurité des données et à les mettre en œuvre de manière exemplaire pour garantir la confidentialité, l'intégrité et la disponibilité des informations personnelles de nos utilisateurs. En optant pour ces mesures de sécurité robustes, nous montrons notre engagement envers la protection des données, assurant ainsi à nos utilisateurs une expérience en ligne des plus sécurisée.            </p>
+
+        </div>
+    </div>
+    <div class="clear"></div>
+</section>
+<br>
+<br>
+<section>
+    <div class="Toute-la-page">
+        <div class="bas-page">
+            <div class="block2-transparent">
+                <h3 style="color: #FFFFFF">5 ) Conclusion</h3>
+                <p>
+                    En conclusion, le Règlement Général sur la Protection des Données (RGPD) est un ensemble de règles qui encadre le traitement des données personnelles des citoyens de européen, par les entreprises. En application depuis 2018, il vise à garantir la protection de ces données, à renforcer les droits individuels et à imposer une responsabilité accrue aux organisations qui traitent ces données. Le RGPD y arrive en annonçant de lourdes sanctions à ceux qui ne les respectent pas.
+                    <br>
+                    <br>
+                    Dans le cadre de notre application, nous faisons en sorte que celle-ci soit bien conforme aux principes du RGPD en appliquant le principe de “privacy by design”. Les informations personnelles collectées sont limitées à l'essentiel pour une meilleure expérience utilisateur, et nous veillerons à assurer une gestion sécurisée en accord avec les droits des individus évoqués dans le RGPD tels que l'accès, la rectification, l'eﬀacement, la portabilité et l'opposition.
+                    <br>
+                    <br>
+                    La sécurité des données personnelles est au cœur de notre démarche, avec un accès restreint par identifiant utilisateur et mot de passe ainsi que le cryptage des données. De cette manière, notre application s'aligne sur les normes du RGPD, contribuant à renforcer la confiance dans l'économie numérique et à garantir la protection des données personnelles de nos utilisateurs.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="clear"></div>
+</section>
+<section>
+    <div class="sources-container">
+        <h3 class="section-title">5) Sources</h3>
+        <p class="source">
+            <span class="source-title">CNIL :</span>
+            <a href="https://www.cnil.fr/fr/professionnel" class="link" target="_blank">https://www.cnil.fr/fr/professionnel</a>
+        </p>
+
+        <p class="source">
+            <span class="source-title">CNIL - Cartographie :</span>
+            <a href="https://www.cnil.fr/fr/cartographier-vos-traitements-de-donnees-personnelles" class="link" target="_blank">https://www.cnil.fr/fr/cartographier-vos-traitements-de-donnees-personnelles</a>
+        </p>
+
+        <p class="source">
+            <span class="source-title">CNIL - Principes clés :</span>
+            <a href="https://www.cnil.fr/principes-cles/guide-de-la-securite-des-donnees-personnelles" class="link" target="_blank">https://www.cnil.fr/principes-cles/guide-de-la-securite-des-donnees-personnelles</a>
+        </p>
+
+        <p class="source">
+            <span class="source-title">CNIL - Durée de conservation :</span>
+            <a href="https://www.cnil.fr/fr/passer-laction/les-durees-de-conservation-des-donnees" class="link" target="_blank">https://www.cnil.fr/fr/passer-laction/les-durees-de-conservation-des-donnees</a>
+
+        </p>
+
+        <p class="source">
+            <span class="source-title">Le monde informatique :</span>
+            <a href="https://www.lemondeinformatique.fr/actualites/lire-rgpd-facebook-ecope-d-une-amende-record-de-1-2-mdeteuro-90480.html" class="link" target="_blank">
+                https://www.lemondeinformatique.fr/actualites/lire-rgpd-facebook-ecope-d-une-amende-record-de-1-2-mdeteuro-90480.html
+            </a>Cours sur le RGPD.
+        </p>
+
+    </div>
+</section>
+
+<?php
+include('../HTML/pied.html');
+include('../HTML/pied_lien.html');
+?>
+</body>
+</html>
