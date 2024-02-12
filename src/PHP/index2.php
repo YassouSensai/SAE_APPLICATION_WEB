e82f28233b3fdc338ea862a99624834870d63a26
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2024-02-12 16:34:51 +0100

    Campage de tests pour verifier les beugs des commandes préaprées :
    toujours le meme test qu'avant, essai 3

--- src/PHP/index2.php
+++ src/PHP/index2.php
@@ -16,11 +16,7 @@ echo "<h1>Liste des Tickets</h1>";
 echo "<strong>Etape Connexion</strong>";
 $connexion = connectDB();
 
-$query = "SELECT t.date_crea_tic, t.objet, u.identifiant AS createur, t.desc_pb_tic, t.adresse_ip, n.libelle_nv_urgence
-              FROM Ticket t
-              JOIN Utilisateur u ON t.createur_tic = u.identifiant
-              JOIN NiveauUrgence n ON t.nv_urgence_tic = n.id_nv_urgence
-              ORDER BY t.date_crea_tic DESC";
+$query = "SELECT objet FROM Ticket ORDER BY date_crea_tic DESC";
 
 // Etape execution de la requete
 echo "<strong>Etape execution de la requete</strong>";
@@ -29,24 +25,14 @@ $resultat = prepareAndExecute($connexion, $query);
 echo "<table border='1'>
                 <thead>
                     <tr>
-                        <th>Date de création</th>
-                        <th>Objet</th>
-                        <th>Information</th>
+                        <th>Nom du ticket</th>
                     </tr>
                 </thead>
                 <tbody>";
 
 while ($row = mysqli_fetch_assoc($resultat)) {
     echo "<tr>";
-    echo "<td>" . htmlspecialchars($row['date_crea_tic']) . "</td>";
     echo "<td>" . htmlspecialchars($row['objet']) . "</td>";
-
-    // Construction dynamique du paragraphe d'information
-    $infoParagraph = "Créé le " . htmlspecialchars($row['date_crea_tic']) . " par l'utilisateur " . htmlspecialchars($row['createur']);
-    $infoParagraph .= " depuis l'adresse IP " . htmlspecialchars($row['adresse_ip']) . ". Urgence : " . htmlspecialchars($row['libelle_nv_urgence']);
-    $infoParagraph .= "<br><br><strong>Problème :</strong> " . htmlspecialchars($row['desc_pb_tic']);
-
-    echo "<td>" . $infoParagraph . "</td>";
     echo "</tr>";
 }
 
