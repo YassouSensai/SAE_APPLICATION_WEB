fa42224760a396929270f3e8de1241ab50c05ab3
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2024-02-12 17:00:37 +0100

    Campage de tests pour verifier les beugs des commandes préaprées :
    test 2
    La fonction prepareandExecute fonction sur le fichier de test index2.php, test sur la page de connexion

--- src/PHP/Authentification/action_connexion.php
+++ src/PHP/Authentification/action_connexion.php
@@ -4,24 +4,29 @@ session_start();
 include("../Autres/fonctions_generales.php");
 include("../Crypto/crypto.php");
 
+echo "<strong>Etape de démarrage</strong><br>";
+
 if (isset($_SESSION['nb1']) && isset($_SESSION['nb2'])) {
     if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['captcha']) && isset($_POST['user-type'])) {
         // Récupération des données de connexion utilisateur
+        echo "<strong>Etape de récupération des données de connexion utilisateur</strong><br>";
         $username = htmlspecialchars($_POST['username']);
         $captcha = htmlspecialchars($_POST['captcha']);
         $table_user = htmlspecialchars($_POST['user-type']);
 
-        $password = RC4("password",htmlspecialchars($_POST['password']));
+        $password = RC4("password", htmlspecialchars($_POST['password']));
 
         if (verifCaptcha($captcha)) {
+            echo "<strong>Etape de vérification du captcha</strong><br>";
             $connexion = connectDB();
 
-            $query = "SELECT * FROM ".$table_user." WHERE identifiant = ? AND mdp = ?";
+            $query = "SELECT * FROM " . $table_user . " WHERE identifiant = ? AND mdp = ?";
             $params = ["ss", $username, $password];
 
             $resultat = prepareAndExecute($connexion, $query, $params);
 
             if (mysqli_num_rows($resultat) > 0) {
+                echo "<strong>Etape de vérification des informations de connexion</strong><br>";
                 $_SESSION['utilisateur'] = $username;
                 $_SESSION['table_user'] = $table_user;
 
@@ -56,4 +61,3 @@ if (isset($_SESSION['nb1']) && isset($_SESSION['nb2'])) {
     exit;
 }
 ?>
-
