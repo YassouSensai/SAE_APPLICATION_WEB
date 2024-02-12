90626fedd74363b2b6190ef04243e1fedd69baf2
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2024-02-12 16:17:14 +0100

    Campage de tests pour verifier les beugs des commandes préaprées :
    toujours le meme test qu'avant

--- src/PHP/index2.php
+++ src/PHP/index2.php
@@ -1,10 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des Tickets</title>
+    <link rel="stylesheet" href="../CSS/css_fonctions.css">
+</head>
+<body>
 <?php
 include("Autres/fonctions_generales.php");
 
-echo "\n\nEtape de départ";
-echo "<link rel='stylesheet' href='../CSS/css_fonctions.css'>";
+echo "<h1>Liste des Tickets</h1>";
 
-echo "\n\nEtape Connexion";
+// Etape Connexion
+echo "<strong>Etape Connexion</strong>";
 $connexion = connectDB();
 
 $query = "SELECT t.date_crea_tic, t.objet, u.identifiant AS createur, t.desc_pb_tic, t.adresse_ip, n.libelle_nv_urgence
@@ -13,18 +22,19 @@ $query = "SELECT t.date_crea_tic, t.objet, u.identifiant AS createur, t.desc_pb_
               JOIN NiveauUrgence n ON t.nv_urgence_tic = n.id_nv_urgence
               ORDER BY t.date_crea_tic DESC";
 
-echo "\n\nEtape execution de la requete";
+// Etape execution de la requete
+echo "<strong>Etape execution de la requete</strong>";
 $resultat = prepareAndExecute($connexion, $query);
 
 echo "<table border='1'>
-            <thead>
-                <tr>
-                    <th>Date de création</th>
-                    <th>Objet</th>
-                    <th>Information</th>
-                </tr>
-            </thead>
-            <tbody>";
+                <thead>
+                    <tr>
+                        <th>Date de création</th>
+                        <th>Objet</th>
+                        <th>Information</th>
+                    </tr>
+                </thead>
+                <tbody>";
 
 while ($row = mysqli_fetch_assoc($resultat)) {
     echo "<tr>";
@@ -42,6 +52,8 @@ while ($row = mysqli_fetch_assoc($resultat)) {
 
 echo "</tbody></table>";
 
-
-echo "\n\nEtape execution de la requete réussie !!!";
+echo "<p><strong>Etape execution de la requete réussie !!!</strong></p>";
 mysqli_close($connexion);
+?>
+</body>
+</html>
