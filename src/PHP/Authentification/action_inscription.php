0f840725bc8cfe458b8a6b4e8144e5e680b92d60
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2024-02-12 17:07:15 +0100

    Campage de tests pour verifier les beugs des commandes préaprées :
    test 3
    La fonction prepareandExecute fonction sur le fichier de test index2.php, test sur la page de connexion non réussie j'essaie avec la page d'inscription

--- src/PHP/Authentification/action_inscription.php
+++ src/PHP/Authentification/action_inscription.php
@@ -4,30 +4,34 @@ session_start();
 include("../Autres/fonctions_generales.php");
 include("../Crypto/crypto.php");
 
+echo "<strong>Etape de démarrage</strong><br>";
+
 if (isset($_SESSION['nb1']) && isset($_SESSION['nb2'])) {
 
     if (isset($_POST['username'], $_POST['nom'], $_POST['prenom'], $_POST['email'], $_POST['password'], $_POST['confirm_password'], $_POST['type_util'], $_POST['captcha'])) {
         // Récupération des données d'inscription
+        echo "<strong>Etape de récupération des données d'inscription</strong><br>";
         $username = htmlspecialchars($_POST['username']);
         $nom = htmlspecialchars($_POST['nom']);
         $prenom = htmlspecialchars($_POST['prenom']);
         $email = htmlspecialchars($_POST['email']);
-        $password = RC4("password",htmlspecialchars($_POST['password']));
-        $confirm_password = RC4("password",htmlspecialchars($_POST['confirm_password']));
+        $password = RC4("password", htmlspecialchars($_POST['password']));
+        $confirm_password = RC4("password", htmlspecialchars($_POST['confirm_password']));
         $type_util = htmlspecialchars($_POST['type_util']);
         $captcha = htmlspecialchars($_POST['captcha']);
 
         if (verifCaptcha($captcha) && ($password === $confirm_password)) {
+            echo "<strong>Etape de vérification du captcha et des mots de passe</strong><br>";
             $connexion = connectDB();
 
             $paramsVerif = ['s', $username];
-            $queryVerif = "SELECT * FROM Utilisateur WHERE identifiant = ?";
+            $queryVerif = "SELECT * FROM utilisateur WHERE identifiant = ?";
             $resultatVerif = prepareAndExecute($connexion, $queryVerif, $paramsVerif);
 
             if (mysqli_num_rows($resultatVerif) > 0) {
                 header('Location: inscription.php?err=err_inscription_identifiant');
             } else {
-                $query = "INSERT INTO Utilisateur (identifiant, nom_util, prenom_util, email_util, mdp, type_util) VALUES (?, ?, ?, ?, ?, ?)";
+                $query = "INSERT INTO utilisateur (identifiant, nom_util, prenom_util, email_util, mdp, type_util) VALUES (?, ?, ?, ?, ?, ?)";
                 $params = ['ssssss', $username, $nom, $prenom, $email, $password, $type_util];
                 $resultat = prepareAndExecute($connexion, $query, $params);
 
