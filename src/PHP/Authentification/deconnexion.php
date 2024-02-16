13cb81cfcc768e7f19fd22b7136e70a79dcafdfb
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2024-02-16 11:06:45 +0100

    Encore des problemes malheureusement.

--- src/PHP/Authentification/deconnexion.php
+++ src/PHP/Authentification/deconnexion.php
@@ -4,7 +4,7 @@ include("../Autres/fonctions_generales.php");
 
 if (isset($_SESSION['utilisateur'])) {
     $username = $_SESSION['utilisateur'];
-    logActivity($username, 1, "L'utilisateur $username s'est déconnecté.");
+    //logActivity($username, 1, "L'utilisateur $username s'est déconnecté.");
 
 
     session_unset();
