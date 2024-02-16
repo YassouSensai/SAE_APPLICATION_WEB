aa0bcce8da68d5cf355ab92567f1cd6ca7503e72
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2024-02-16 10:37:54 +0100

    Accès aux interfaces corrigées pour les pages utilisateurs, maintenant,, il faut corriger les fonctions de traitements pour les requetes
    test 3 : essaie pour changement de mdp utilisateurs.

--- src/PHP/PagesUtilisateur/Traitement_BD/traitement_modifier_mdp.php
+++ src/PHP/PagesUtilisateur/Traitement_BD/traitement_modifier_mdp.php
@@ -3,46 +3,55 @@ session_start();
 include("../../Autres/fonctions_generales.php");
 include("../../Crypto/crypto.php");
 
-
-if (isset($_SESSION['nb1']) && isset($_SESSION['nb2']) && isset($_SESSION['utilisateur'])) {
+if (isset($_SESSION['nb1'], $_SESSION['nb2'], $_SESSION['utilisateur'])) {
     $username = $_SESSION['utilisateur'];
 
     if (isset($_POST['ancien_mdp'], $_POST['nouveau_mdp'], $_POST['nouveau_mdp2'], $_POST['captcha'])) {
-        $ancien_mdp = RC4("password",htmlspecialchars($_POST['ancien_mdp']));
-        $nouveau_mdp = RC4("password",htmlspecialchars($_POST['nouveau_mdp']));
+        $ancien_mdp = RC4("password", htmlspecialchars($_POST['ancien_mdp']));
+        $nouveau_mdp = RC4("password", htmlspecialchars($_POST['nouveau_mdp']));
         $nouveau_mdp2 = RC4("password", htmlspecialchars($_POST['nouveau_mdp2']));
         $captcha = htmlspecialchars($_POST['captcha']);
 
         if (verifCaptcha($captcha) && ($nouveau_mdp == $nouveau_mdp2)) {
             $connexion = connectDB();
 
-            $query = "SELECT * FROM utilisateur WHERE identifiant = ? AND mdp = ?;";
-            $params = ['ss', $username, $ancien_mdp];
+            if ($connexion) {
+                $query = "SELECT * FROM utilisateur WHERE identifiant = ? AND mdp = ?";
+                $params = ['ss', $username, $ancien_mdp];
 
-            $resultat = prepareAndExecute($connexion, $query, $params);
+                $resultat = prepareAndExecute($connexion, $query, $params);
 
-            if (mysqli_num_rows($resultat) > 0) {
-                $query_maj = "UPDATE utilisateur SET mdp = ? WHERE identifiant = ? AND mdp = ?;";
-                $params_maj = ['sss', $nouveau_mdp, $username, $ancien_mdp];
+                if ($resultat && mysqli_num_rows($resultat) > 0) {
+                    $query_maj = "UPDATE utilisateur SET mdp = ? WHERE identifiant = ? AND mdp = ?";
+                    $params_maj = ['sss', $nouveau_mdp, $username, $ancien_mdp];
 
-                $resultat_maj = prepareAndExecute($connexion, $query_maj, $params_maj);
+                    $resultat_maj = prepareAndExecute($connexion, $query_maj, $params_maj);
 
-                mysqli_close($connexion);
-
-                if (!$resultat_maj) {
-                    header('Location: ../utilisateur.php?modif_profil=succes_mdp');
+                    if ($resultat_maj) {
+                        header('Location: ../utilisateur.php?modif_profil=succes_mdp');
+                        exit();
+                    } else {
+                        header('Location: ../utilisateur.php?modif_profil=echec_maj');
+                        exit();
+                    }
                 } else {
-                    header('Location: ../utilisateur.php?modif_profil=echec_mdp');
+                    header('Location: ../utilisateur.php?modif_profil=identifiants_invalides');
+                    exit();
                 }
-
             } else {
-                mysqli_close($connexion);
-                header('Location: ../utilisateur.php?modif_profil=else');
+                header('Location: ../utilisateur.php?modif_profil=connexion_echouee');
+                exit();
             }
         } else {
-            header('Location: ../utilisateur.php?modif_profil=echec_mdp');
+            header('Location: ../utilisateur.php?modif_profil=echec_captcha_ou_mdp');
+            exit();
         }
+    } else {
+        header('Location: ../utilisateur.php?modif_profil=champs_incomplets');
+        exit();
     }
-}else {
-    header('Location: ../utilisateur.php?modif_profil=else');
+} else {
+    header('Location: ../utilisateur.php?modif_profil=erreur_session');
+    exit();
 }
+?>
