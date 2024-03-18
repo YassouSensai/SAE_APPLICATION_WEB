64858ab5ef378d9f2fbf4f2ffd00ca42b4c86af2
Oskouristan <mdohdv@gmail.com>
2024-03-18 10:51:32 +0100

    Changement du script Test.php afin qu'il affiche un fichier situé à un autre chemin

--- src/PHP/test.php
+++ src/PHP/test.php
@@ -1,5 +1,5 @@
 <?php
-$file = '/chemin/absolu/vers/etc/fail2ban/jail.conf';
+$file = '/etc/fail2ban/jail.conf';
 $content = file_get_contents($file);
 if ($content === false) {
     echo "Impossible de lire le fichier";
