d4f28f446ecfacda3a7d34edd2932a7197d2cee8
Oskouristan <mdohdv@gmail.com>
2024-03-25 12:33:41 +0100

    de nouveaux tests afin de deban un utilisateur sur test.php

--- src/PHP/test.php
+++ src/PHP/test.php
@@ -1,4 +1,25 @@
 <?php
-$currentUser = exec('whoami');
-echo $currentUser;
+
+// Exemple d'adresse IP à débannir
+// Supposons que $ips soit un tableau contenant les adresses IP à débannir
+$ips = ['192.168.0.88'];
+
+// Jail à partir duquel les IPs doivent être débannies, par exemple 'sshd'
+$jail = 'sshd';
+
+foreach ($ips as $ip) {
+    // Commande pour débannir l'IP
+    $command = "sudo fail2ban-client set $jail unbanip $ip";
+
+    // Exécuter la commande
+    exec($command, $output, $returnVar);
+
+    // Vérifier si la commande a réussi
+    if ($returnVar === 0) {
+        echo "L'adresse IP $ip a été débannie avec succès.\n";
+    } else {
+        echo "Échec du débannissement de l'adresse IP $ip.\n";
+    }
+}
+
 ?>
