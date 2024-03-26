c1549f73e82ce28f831257322cd9ba71e64fd40b
Oskouristan <mdohdv@gmail.com>
2024-03-26 09:50:55 +0100

    changement de la fonction_deban_ip.php

--- save_and_clear_fail2ban_log.sh
+++ save_and_clear_fail2ban_log.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-#Chemin vers le répertoire de destination
+# Chemin vers le répertoire de destination
 DEST_DIR="/home/pisae/Documents/SAE_APPLICATION_WEB/csv"
 
-#Création du répertoire de destination s'il n'existe pas
+# Création du répertoire de destination s'il n'existe pas
 mkdir -p "$DEST_DIR"
 
-#Format de la date pour nommer le fichier
-DATE=$(date +'%Y-%m-%d%H-%M-%S')
+# Format de la date pour nommer le fichier
+DATE=$(date +'%Y-%m-%d_%H-%M-%S')
 
-#Nom du fichier ZIP de destination
-ZIPFILE="$DESTDIR/csv_backup$DATE.zip"
+# Nom du fichier ZIP de destination
+ZIPFILE="$DEST_DIR/csv_backup_$DATE.zip" # Correction: Utilisation de la variable $DEST_DIR correctement
 
-#Compresser tous les fichiers CSV existants dans le répertoire csv
-zip -j "$ZIPFILE" "$DESTDIR"/.csv
+# Compresser tous les fichiers CSV existants dans le répertoire csv
+zip -j "$ZIPFILE" "$DEST_DIR"/*.csv # Correction: Chemin correct pour matcher les fichiers CSV
 
-#Supprimer les fichiers CSV après compression
-rm "$DEST_DIR"/.csv
+# Supprimer les fichiers CSV après compression
+rm "$DEST_DIR"/*.csv # Correction: Chemin correct pour matcher et supprimer les fichiers CSV
 
-#Nom du nouveau fichier CSV
-CSVFILE="$DEST_DIR/ip_banned$DATE.csv"
+# Nom du nouveau fichier CSV
+CSVFILE="$DEST_DIR/ip_banned_$DATE.csv" # Ajouté underscore pour la cohérence
 
-#Exécuter la commande et sauvegarder le résultat dans le nouveau fichier CSV
+# Exécuter la commande et sauvegarder le résultat dans le nouveau fichier CSV
 sudo fail2ban-client status sshd > "$CSVFILE"
