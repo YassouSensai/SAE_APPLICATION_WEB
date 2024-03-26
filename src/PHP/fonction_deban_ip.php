b5b1361c37daa674e52914acd456f6e644bbf11c
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2024-03-26 11:18:55 +0100

    mja reussi deban ip

--- src/PHP/fonction_deban_ip.php
+++ src/PHP/fonction_deban_ip.php
@@ -1,10 +1,11 @@
 <?php
 if(isset($_GET['ip'])) {
     $ip = $_GET['ip'];
-
 } else {
     echo "<script>alert('Erreur : Aucune adresse IP fournie.');</script>";
+    exit; // Quitte le script si aucune adresse IP n'est fournie
 }
+
 function debannirIP($ip, $jail = 'sshd') {
     $command = "sudo /usr/bin/fail2ban-client set $jail unbanip $ip 2>&1";
 
@@ -15,21 +16,17 @@ function debannirIP($ip, $jail = 'sshd') {
     } else {
         echo "<script>alert('Échec du débannissement de l\'adresse IP $ip.');</script>";
     }
-    //$cheminScript = '../../../save_and_clear_fail2ban_log.sh';
-    $returnVarScript = shell_exec("../../../save_and_clear_fail2ban_log.sh");
-    //exec($cheminScript, $outputScript, $returnVarScript);
+
+    $cheminScript = '../../../save_and_clear_fail2ban_log.sh';
+    $returnVarScript = shell_exec($cheminScript);
 
     if ($returnVarScript === 0) {
         echo "<script>alert('Le script save_and_clear_fail2ban_log.sh a été exécuté avec succès.');</script>";
     } else {
         echo "<script>alert('Échec de l\'exécution du script save_and_clear_fail2ban_log.sh.');</script>";
     }
-
-    //foreach ($outputScript as $line) {
-    //    echo "<script>alert('$line');</script>";
-    //}
-
 }
+
 debannirIP($ip);
 echo "<script>window.location.href = 'PagesUtilisateur/tableau_de_bord_utilisateur.php?journal=rpi';</script>";
 ?>
