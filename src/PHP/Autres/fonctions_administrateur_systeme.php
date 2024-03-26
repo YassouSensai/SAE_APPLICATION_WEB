21b088fb49da9031a9448ece6c0078310fda280a
22203744 <ostap.tymchyshyn.92@gmail.com>
2024-03-26 10:29:09 +0100

    (REUSSI) : implémentation de la fonction debannirIP qui deban les ip de ceux se trovant dans le tableau des bannis quand on clique sur la petite poubelle pour debannir.En effet il y a quelques problèmes

--- src/PHP/Autres/fonctions_administrateur_systeme.php
+++ src/PHP/Autres/fonctions_administrateur_systeme.php
@@ -6,34 +6,6 @@
 // ####################################################################################################################
 // ####################################################################################################################
 // ####################################################################################################################
-/**
- * Débannir une adresse IP.
- *
- * @param string $ip L'adresse IP à débannir.
- * @param string $jail Le jail Fail2ban concerné. Par défaut, 'sshd'.
- * @return bool True si l'adresse IP est débannie avec succès, sinon False.
- */
-function debannirIP($ip, $jail = 'sshd') {
-    $command = "sudo /usr/bin/fail2ban-client set $jail unbanip $ip 2>&1";
-
-    exec($command, $output, $returnVar);
-
-    if ($returnVar === 0) {
-        echo "L'adresse IP $ip a été débannie avec succès.\n";
-    } else {
-        echo "Échec du débannissement de l'adresse IP $ip.\n";
-    }
-    $cheminScript = '/../../save_and_clear_fail2ban_log.sh';
-
-    exec("sudo bash $cheminScript", $outputScript, $returnVarScript);
-
-    if ($returnVarScript === 0) {
-        echo "Le script save_and_clear_fail2ban_log.sh a été exécuté avec succès.\n";
-    } else {
-        echo "Échec de l'exécution du script save_and_clear_fail2ban_log.sh.\n";
-    }
-}
-
 
 /**
  * Cette fonction lit un fichier CSV contenant les adresses IP bannies et retourne la liste des adresses IP.
